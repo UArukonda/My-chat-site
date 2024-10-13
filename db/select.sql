fc2eec175e55214ad5dddc979163adde0ecc8fd2
UArukonda <uarukonda@gmail.com>
2024-10-13 01:45:40 +0100

    this commit will create a dev and test databases

---
+++ db/select.sql
@@ -0,0 +1,7 @@
+\c chatapp_dev_db
+
+\dt
+
+\c chatapp_test_db
+
+\dt
---
+++ db/setup.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS chatapp_dev_db;
+CREATE DATABASE chatapp_dev_db;
+
+
+DROP DATABASE IF EXISTS chatapp_test_db;
+CREATE DATABASE chatapp_test_db;
